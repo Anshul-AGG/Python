22b2b95ab9adeafee50f324b8db8c37aca289dd5
Anshul <anshulagarwal393@gmail.com>
2025-11-04 16:27:53 +0530

    loop practice

---
+++ ApnaCollege/for.py
@@ -0,0 +1,51 @@
+# print the elements of the list using for loop
+
+# nums = [1, 2, 4, 5, 6 ,6,7,8,]
+
+# for i in nums:
+#     print(i)
+
+# search for number x in a tuple
+
+# tup = (1,23,4,56,7,3,66,7)
+# x = 7
+# idx = 0
+# for i in tup:
+#     if(i == x):
+#         print("x Found at index ", idx)
+#     idx += 1
+        
+# using for and range
+# print numbers from 1 to 100
+#
+# for i in range(1, 101):
+#     print(i)  
+
+# print numbers from 100 to 1
+# for i in range(100, 0):
+#     print(i)
+
+#print the multiplication table of num n
+
+# n = 5
+# for i in range(11):
+#     print(i*n)
+ 
+# find the factorial of first n number
+
+# n = int(input("Enter a number : "))
+
+# count = 1
+
+# for i in range(1, n):
+#     count *= i+1
+# print("The factorial of " , n,  "is " , count) 
+
+#find the sum of first n numbers
+
+# n = int(input("Enter the number : "))
+# sum = 0
+# for i in range(1, n+1):
+#     sum+=i
+#     print(sum)
+# print("sum of first ", n , "number is " , sum)
---
+++ ApnaCollege/while.py
@@ -0,0 +1,57 @@
+# print from 1 to 100
+
+# i = 1
+
+# while i <= 100:
+#     print(i)
+#     i += 1
+
+# print from 100 to 1
+
+# i = 100
+
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+
+# multiplication table of number n
+# i = 1
+# n = int(input("Enter any number: "))
+# while i <= 10:
+#     print(n*i)
+#     i+=1
+
+#print the elements of the list using while loop
+
+# nums = [1,4,9,14,25,36,50,90,100]
+# ind = 0
+
+# while ind < len(nums):
+#     print(nums[ind])
+#     ind += 1    
+
+
+#find the sum of first n numbers
+
+# n = int(input("Enter the positive number : "))
+
+# count = 1
+# sum = 0
+
+# while count <= n:
+#     sum += count
+#     count += 1
+#     print(sum)
+
+#find the factorial 
+
+# n = int(input("Enter the number: "))
+# product = 1
+# i = 1
+
+# while i <= n:
+#     product *= i
+#     i += 1
+#     print(product)
+    
